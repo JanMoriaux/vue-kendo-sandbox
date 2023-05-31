103be66ccc4251ab80fafd7c5d03553287b45f14
janmoriaux <17312788+JanMoriaux@users.noreply.github.com>
2023-05-31 08:28:11 +0000

    Added grid

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Hello worl</div>
+  <grid></grid>
 </template>
+<script setup>
+import "@progress/kendo-theme-default/dist/all.css";
+
+import { Grid } from "@progress/kendo-vue-grid";
+</script>
