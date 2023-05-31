4788b4d8689f3ee2c5567888b27e927558ca8559
rafal <92117516+rafalhogent@users.noreply.github.com>
2023-05-31 09:12:26 +0000

    Update App.vue w602px

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <grid
-    :style="{ height: '558px', width: '601px' }"
+    :style="{ height: '558px', width: '602px' }"
     :data-items="products"
     :reorderable="true"
     @columnreorder="columnReorder"
